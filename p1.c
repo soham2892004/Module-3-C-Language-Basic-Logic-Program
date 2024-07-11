74a7f975e8267cd0a2e80a6989cdb4ad9c7e0b39
Soham Parmar <sohamparmar79@gmail.com>
2024-07-11 08:35:00 +0530

    initial commit

---
+++ p1.c
@@ -0,0 +1,10 @@
+ #include <stdio.h>
+
+ void main()
+ {
+
+     printf(" name:Parmar Soham Nileshbhai");
+     printf("\n Birth date:28th september,2004");
+     printf("\n Age:20");
+     printf("\n Address:Anand park,'amardeep',Manavadar-362630");
+ }
